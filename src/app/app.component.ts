62aa336eb264b6bd48b3640221a3928e653d834b
kgaogelothorn <nhlalucky9@gmail.com>
2019-10-21 14:19:46 +0200

    refactor(data):subscribed to an api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-import {HttpClientModule, HttpClient} from '@angular/common/http';
-
-
+import { HttpClient} from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,8 +7,35 @@ import {HttpClientModule, HttpClient} from '@angular/common/http';
 })
 export class AppComponent {
   title = 'Randomuser';
+  userData: any = [];
+  showItems: any = [];
 
+  constructor(private http: HttpClient) { }
 
-  constructor(private http: HttpClient) {}
+  ngOnInIt() {
+    this.showItems.name = true ;
+    this.getUserData().subscribe( data => {
+      const user: any = data;
+      this.userData.image = user.results[0].picture.large;
+      this.userData.email = user.results[0].email;
+      this.userData.phone = user.results[0].phone;
+      this.userData.firstname = user.results[0].name.first;
+      this.userData.lastname = user.results[0].name.last;
+      this.userData.location = `${user.results[0].location.street} ${user.results[0].location.city}`;
+      this.userData.password = user.results[0].login.password;
+      this.userData.birthDate = user.results[0].dob.date;
+    }, error => {
+      alert(error);
+    });
+  }
+
+  getUserData() {
+    const options = {headers: {'Content-Type': 'application/json'}};
+    return this.http.get('https://randomuser.me/api/', options );
+  }
+
+  show(item) {
+    this.showItems = [];
+    this.showItems[item] = true;
+  }
 }
-
