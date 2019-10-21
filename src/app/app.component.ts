82fcc05e508713139545eb78365ddd73e2feda16
kgaogelothorn <nhlalucky9@gmail.com>
2019-10-21 20:31:03 +0200

    refactor(data):subscribed to an api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent  {
   title = 'Randomuser';
   userData: any = [];
   showItems: any = [];
 
-  constructor(private http: HttpClient) { }
+  constructor(readonly http: HttpClient) {
+    console.log('constructor');
+    this.ngOnIniIt();
+   }
 
-  ngOnInIt() {
+  ngOnIniIt() {
+    console.log('oninit');
     this.showItems.name = true ;
-    this.getUserData().subscribe( data => {
-      const user: any = data;
+    this.getUserData().subscribe((user: any) => {
+      // debugger
+      console.log('data', user);
       this.userData.image = user.results[0].picture.large;
       this.userData.email = user.results[0].email;
       this.userData.phone = user.results[0].phone;
       this.userData.firstname = user.results[0].name.first;
       this.userData.lastname = user.results[0].name.last;
-      this.userData.location = `${user.results[0].location.street} ${user.results[0].location.city}`;
+      this.userData.location = `${user.results[0].location.street.name} ${user.results[0].location.city}`;
       this.userData.password = user.results[0].login.password;
       this.userData.birthDate = user.results[0].dob.date;
     }, error => {
@@ -34,8 +39,8 @@ export class AppComponent {
     return this.http.get('https://randomuser.me/api/', options );
   }
 
-  show(item) {
+  show(element) {
     this.showItems = [];
-    this.showItems[item] = true;
+    this.showItems[element] = true;
   }
 }
